f1fe77c8922238beb31be21cf557f1bfb76a86cb
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-04 14:33:02 +0100

    Initial commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,11 +7,12 @@ if number < 0:
     print(f"{-last_d}", end=" ")
 else:
     print(f"{last_d}", end=" ")
-if last_d > 5:
+
+if last_d == {-last_d}:
+    print("and is less than 6 and not 0")
+elif last_d > 5:
     print("and is greater than 5")
 elif last_d == 0:
     print("and is 0")
-elif last_d == {-last_d}:
-    print("and is less than 6 and not 0")
 else:
     print("and is less than 6 and not 0")
