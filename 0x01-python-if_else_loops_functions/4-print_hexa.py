4ab5d18f5587549a30b69d06167079d739248cdb
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-04 21:36:56 +0100

    python programming

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-    dec_hex = (i) + "=" hexadecimal value
-    print("{}".format(dec_hex))
+    dec_hex = (i, hex(i))
+    print("{} = {}".format(i, hex(i)))
