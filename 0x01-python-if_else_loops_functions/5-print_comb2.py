5c571d30c1bccc0cef150982ed6b73b1d59eb945
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-05 04:32:52 +0100

    if statement

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{}{}".format(i, ","), end=" ")
-    
-if i == 99:
-    print()
+    print("{:02d}{}".format(i, ","), end=" ")
+if i < 99:
+    print("{}".format(i), end="")
