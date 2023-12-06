62e44a66b5346aeaf3210c4bb6e4ef78771f249e
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-06 05:11:10 +0100

    python programming

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#for i in range(0, 100):
+#    if i != 99:
+#        print("{:02d}, ".format(i), end="")
+#    else:
+#        print("{}".format(99), end="\n")
+
+for i in range(0, 10):
+    for i in range(0, 10):
+        print(i)
