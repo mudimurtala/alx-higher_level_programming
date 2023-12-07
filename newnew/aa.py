3faed2507522022976092278a657887a8be364db
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-07 13:53:20 +0100

    python programming

--- newnew/aa.py
+++ newnew/aa.py
@@ -14,11 +14,11 @@ set_b = {3, 4, 5, 6, 7}
 symmetric_difference_set = set_a.symmetric_difference(set_b)
 difference_update_set = set_a.difference_update(set_b)
 disjoint_set = set_a.isdisjoint(set_b)
-forzen_set = frozenset({1, 2, 3, 4, 5})
+frozen_set = frozenset([1, 2, 3, 4, 5])
 
 
 
-print(frozenset)
+print(frozen_set)
 print(disjoint_set)
 print(difference_update_set)
 print(symmetric_difference_set)
