830a4c007ca3ad9803ef8590432362031cf161d5
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-07 12:36:45 +0100

    python programming

--- newnew/aa.py
+++ newnew/aa.py
@@ -4,6 +4,15 @@ my_set.add(5)
 my_set.remove(3)
 result = 8 in my_set
 other_set = {3, 4, 5, 6}
+union_set = my_set.union(other_set)
+intersection_set = my_set.intersection(other_set)
+
+
+
+
+
+print(intersection_set)
+print(union_set)
 print(other_set)
 print(result)
 print(my_set)
