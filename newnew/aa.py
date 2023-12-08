4ef76bef60d5d9e93958dd3e7604ac0fe8eca845
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-08 16:48:26 +0100

    python programming

--- newnew/aa.py
+++ newnew/aa.py
@@ -1,123 +1,141 @@
-#!/usr/bin/python3
-my_set = set([1, 2, 3, 4])
-my_set.add(5)
-my_set.remove(3)
-result = 8 in my_set
-other_set = {3, 4, 5, 6}
-union_set = my_set.union(other_set)
-intersection_set = my_set.intersection(other_set)
-difference_set = my_set.difference(other_set)
-subset_set = my_set.issubset(other_set)
-my_set_copy = my_set.copy()
-set_a = set([1, 2, 3, 4, 5])
-set_b = {3, 4, 5, 6, 7}
-symmetric_difference_set = set_a.symmetric_difference(set_b)
-difference_update_set = set_a.difference_update(set_b)
-disjoint_set = set_a.isdisjoint(set_b)
-frozen_set = frozenset({1, 2, 3, 4, 5})
-square_set = {x**2 for x in range(1, 11)}
-original_list = [1, 2, 2, 3, 4, 4, 5]
-original_list = set(original_list)
-set_c = set(['apple', 'banana', 'orange'])
-set_d = set(['banana', 'orange', 'kiwi'])
-common_elements = set_c.intersection(set_d)
+# #!/usr/bin/python3
+# my_set = set([1, 2, 3, 4])
+# my_set.add(5)
+# my_set.remove(3)
+# result = 8 in my_set
+# other_set = {3, 4, 5, 6}
+# union_set = my_set.union(other_set)
+# intersection_set = my_set.intersection(other_set)
+# difference_set = my_set.difference(other_set)
+# subset_set = my_set.issubset(other_set)
+# my_set_copy = my_set.copy()
+# set_a = set([1, 2, 3, 4, 5])
+# set_b = {3, 4, 5, 6, 7}
+# symmetric_difference_set = set_a.symmetric_difference(set_b)
+# difference_update_set = set_a.difference_update(set_b)
+# disjoint_set = set_a.isdisjoint(set_b)
+# frozen_set = frozenset({1, 2, 3, 4, 5})
+# square_set = {x**2 for x in range(1, 11)}
+# original_list = [1, 2, 2, 3, 4, 4, 5]
+# original_list = set(original_list)
+# set_c = set(['apple', 'banana', 'orange'])
+# #x = int(input("Please enter an imteger: "))
+# #if x < 0:
+# #    x = 0
+# #    print("Negative changed to zero")
+# #elif x == 0:
+# #    print("zero")
+# #elif x == 1:
+# #    print("single")
+# #else:
+# #    print("more")
+# set_d = set(['banana', 'orange', 'kiwi'])
+# common_elements = set_c.intersection(set_d)
 
 
 
-#print(common_elements)
-#print(set_c, end="\n")
-#print(set_d)
-#print(original_list)
-#print(original_list)
-#print(square_set)
-#print(frozen_set)
-#print(disjoint_set)
-#rint(difference_update_set)
-#print(symmetric_difference_set)
-#print(set_a, set_b)
-#print(my_set_copy)
-#print(subset_set)
-#print(difference_set)
-#print(intersection_set)
-#print(union_set)
-#print(other_set)
-#print(result)
-#print(my_set)
-
-#x = int(input("Please enter an imteger: "))
-#if x < 0:
-#    x = 0
-#    print("Negative changed to zero")
-#elif x == 0:
-#    print("zero")
-#elif x == 1:
-#    print("single")
-#else:
-#    print("more")
-
-#words = ['cat', 'window', 'defenestrate']
-#for yes in words:
-#    print(yes, len(yes))
-
-# Create a sample collection
-#users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
-
-# Strategy:  Iterate over a copy
-#for user, status in users.copy().items():
-#    if status == 'inactive':
-#        del users[user]
-
-#print(users)
+# # print(common_elements)
+# # print(set_c, end="\n")
+# # print(set_d)
+# # print(original_list)
+# # print(original_list)
+# # print(square_set)
+# # print(frozen_set)
+# # print(disjoint_set)
+# # rint(difference_update_set)
+# # print(symmetric_difference_set)
+# # print(set_a, set_b)
+# #print(my_set_copy)
+# #print(subset_set)
+# #print(difference_set)
+# #print(intersection_set)
+# #print(union_set)
+# #print(other_set)
+# #print(result)
+# #print(my_set)
 
 
+# #words = ['cat', 'window', 'defenestrate']
+# #for yes in words:
+# #    print(yes, len(yes))
+
+# # Create a sample collection
+# #users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# # Strategy:  Iterate over a copy
+# #for user, status in users.copy().items():
+# #    if status == 'inactive':
+# #        del users[user]
+
+# #print(users)    short Alake copy childrenchildrenchildrenchildrenchildren
+
+# children = {'Alani': 'tall', 'Alamu': 'short', 'Alake': 'tall'}
+# children = {'Alani': 'tall', 'Alamu': 'short', 'Alake': 'tall'}
+
+# for child, height in children.copy().items():
+
+
+# for child, height in children.copy().items():
+
+
+# for child, height in adults.copy().items():
+
+# for child, height in adults.copy().items():
+
+
+# for child, height in adults.copy().items():
+
+# tuple
 
 
 
-
-#children = {'Alani': 'tall', 'Alamu': 'short', 'Alake': 'tall'}
-#for child, height in children.copy().items():
-#    if height == 'short':
-#        del children[child]
-#print(children)
+# # Strategy:  Create a new collection
 
 
-# Strategy:  Create a new collection
-#active_users = {}
-#for user, status in users.items():
-#    if status == 'active':
-#         active_users[user] = status
 
-#print(active_users)
-
-#tall_children = {}
-#for child, height in children.items():
-#     if height == 'tall':
-#          tall_children[child] = height
-
-#print(tall_children)
+#                     adults = {'Alani': 'tall', 'Alamu': 'short', 'Alake': 'tall'}
+#                     for child, height in children.copy().items():
+#                     if height == 'short':
+#                     del children[child]
+#                     print(children)
 
 
-#zz = list(range(-10, -100, -30))
-#print(zz)
+# #active_users = {}
+# #for user, status in users.items():
+# #    if status == 'active':
+# #         active_users[user] = status
 
-#a = ['Mary', 'had', 'a', 'little', 'lamb', 2]
-#for items in range(len(a)):
-#    print(items, a[items])
+# #print(active_users)
+
+# #tall_children = {}
+# #for child, height in children.items():
+# #     if height == 'tall':
+# #          tall_children[child] = height
+
+# #print(tall_children)
 
 
-#a = range(6, 15, 3)
-#print(a)
+# #zz = list(range(-10, -100, -30))
+# #print(zz)
 
-age = eval(input("Enter age: "))
+# #a = ['Mary', 'had', 'a', 'little', 'lamb', 2]
+# #for items in range(len(a)):
+# #    print(items, a[items])
 
-if age < 5:
-    print("Too young for School")
 
-elif age == 5:
-    print("Go to Kindergarten")
-elif (age > 5) and (age <= 17):
-    grade = age -5
-    print("Go to {} grade".format(grade))
+# #a = range(6, 15, 3)
+# #print(a)
 
-else:
-    print("Go to collage")
+# age = eval(input("Enter age: "))
+
+# if age < 5:
+#     print("Too young for School")
+
+# elif age == 5:
+#     print("Go to Kindergarten")
+# elif (age > 5) and (age <= 17):
+#     grade = age -5
+#     print("Go to {} grade".format(grade))
+
+# else:
+#     print("Go to collage")
