c8c958ac85741e8f2416d8d1988fb247fa6dcd77
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-09 04:48:57 +0100

    python programming

--- newnew/aa.py
+++ newnew/aa.py
@@ -175,3 +175,12 @@ tree_height = int(tree_height)
 spaces = tree_height - 1
 hashes = 1
 stump_spaces = tree_height - 1
+while tree_height != 0:
+    for i in range(spaces):
+        print(' ', end="")
+    for i in range(hashes):
+        print('#', end="")
+    print()
+    spaces -= 1
+    hashes += 2
+    
