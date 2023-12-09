ca414e5c0eb1cb96c163ea7a952a0586474376dc
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-09 06:46:17 +0100

    python programming

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -8,11 +8,15 @@
 #for i in range(0, 10):
 #    print("{:02d}, ".format(''.join(map(str, i + 1))), end="")
 
+# for i in range(10):
+#     for j in range(i + 1, 10):  # Start j from i+1 to avoid duplicates and self-pairs
+#         combination = f"{i}{j}"
+#         if i < 9 or j < 9:  # Check if it's not the last combination
+#             print(combination, end=", ")
+#         else:
+#             print(combination, end="\n")
+
 for i in range(10):
     for j in range(i + 1, 10):  # Start j from i+1 to avoid duplicates and self-pairs
         combination = f"{i}{j}"
-        if i < 9 or j < 9:  # Check if it's not the last combination
-            print(combination, end=", ")
-        else:
-            print(combination, end="\n")
-print("")
+        print(combination)
