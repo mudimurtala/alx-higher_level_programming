3db5ddfe9516bc2ad890c0bb4669fa9692a43318
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-10 05:09:30 +0100

    python programming

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for ch in range(26):
     if ch % 2 == 0:
-        print("{:c}".format(122 - ch), end=" ")
+        print("{:c}".format(122 - ch), end="")
     else:
-        print("{:c}".format(90 - ch), end=" ")
+        print("{:c}".format(90 - ch), end="")
