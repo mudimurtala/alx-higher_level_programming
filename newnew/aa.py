54a4cea0c2ba1ede855475746c96aea76701935f
mudimurtala <ibnmuhyideen95@gmail.com>
2024-01-05 04:34:14 +0100

    python

--- newnew/aa.py
+++ newnew/aa.py
@@ -301,4 +301,43 @@
 
 
 
+# nearest stars to Earth
+star1 = 'Sol'
+star2 = 'Alpha Centauri'
+star3 = 'Barnard'
+star4 = 'Wolf 359'
+
+
+# nearest peak on each tectonic plate
+African = 'Kilimanjaro'
+Antarctic = 'Vinson'
+Australian = 'Puncak Jaya'
+Eurasian = 'Everest'
+North_America = 'Denali'
+Pacific = 'Mauna Kea'
+South_America = 'Aconcagua'
+
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+
+print(stars[3])
+
+peaks = {
+    'African' : 'Kilimanjaro',
+    'Antarctic' : 'Vinson',
+    'Australian' : 'Puncak Jaya',
+    'Eurasian' : 'Everest',
+    'North_America' : 'Denali',
+    'Pacific' : 'Mauna Kea',
+    'South_America' : 'Aconcagua',
+}
+
+print(peaks['Pacific'])
+
+
+
 
