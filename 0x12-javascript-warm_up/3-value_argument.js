d1082b53abd3b4f3a75acfcf9895a7bd19eaee9d
mudimurtala <ibnmuhyideen95@gmail.com>
2024-04-09 16:20:46 +0100

    Add Task 3 solutions

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+
+const args = process.argv;
+if (args[2]) { console.log(args[2]); } else { console.log('No argument'); }
