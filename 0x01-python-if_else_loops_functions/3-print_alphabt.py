1a0629a7f2a32df59e497457579b6821c597fb65
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-14 12:22:37 +0100

    Review code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123,):
-    alpha = chr(i)
-    if alpha not in ['q', 'e']:
-        print("{}".format(alpha), end="")
+for i in range(97, 123):
+    if i != 113 and i != 101:
+        print("{}".format(chr(i)), end="")
