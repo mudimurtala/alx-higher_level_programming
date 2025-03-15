d3eddd210938f1f7ecc16bc3e7242ec9519783c4
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-15 13:57:27 +0100

    Review code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num != 99:
-        print("{:02d}, ".format(num), end="")
+        print(f"{num:02d}, ", end="")
     else:
-        print("{:02d}".format(num))
+        print(f"{num:02d}")
