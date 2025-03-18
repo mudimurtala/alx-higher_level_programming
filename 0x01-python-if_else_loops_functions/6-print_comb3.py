3b481bf7f5466fe51300786cea09233b1b1affe8
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-18 14:08:10 +0100

    Review solution

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+for i in range(0, 10):
+    for j in range(i + 1, 10):  # Start j from i + 1 to ensure unique pairs
+        if i == 8 and j == 9:  # Ensuring no trailing comma for last pair
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
