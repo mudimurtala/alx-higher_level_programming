167179c47c4f25234c49422f0c51e6730996ab4c
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-18 17:20:48 +0100

    Review solution

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=",")
+def uppercase(str):
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print(i, end="")
+    print()
