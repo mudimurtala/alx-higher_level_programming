107db4187c74b62675ca728e6f5c66f9d6ba79d9
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-18 15:21:20 +0100

    Review solution

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
+uppercase = __import__('6-print_comb3').uppercase
 
 uppercase("best")
 uppercase("Best School 98 Battery street")
