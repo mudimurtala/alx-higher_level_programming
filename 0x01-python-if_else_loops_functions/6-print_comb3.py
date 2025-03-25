c2b5486e6830e8fa4e1ccfe1643a649fbccd4c62
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-25 12:36:29 +0100

    Review solution

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             i = 'Fizz'
-            print("{}".format(i), end=" ")
+        print("{}".format(i), end=" ")
 
 fizzbuzz()
