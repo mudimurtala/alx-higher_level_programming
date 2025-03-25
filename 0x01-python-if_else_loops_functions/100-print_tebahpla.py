a313874ca008a74752c83a384153740ddf574261
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-25 18:29:49 +0100

    Review solution

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,3 +4,11 @@ for ch in range(26):
         print("{:c}".format(122 - ch), end="")
     else:
         print("{:c}".format(90 - ch), end="")
+
+
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i - 32)), end="")
