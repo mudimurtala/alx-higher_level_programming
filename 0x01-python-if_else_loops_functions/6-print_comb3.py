01efb0c4e7ac00f3378244a8981a7bf255589e03
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-26 17:53:15 +0100

    Add code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print("{}".format(chr(i)), end="")
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return str
+    return str[:n] + str[n+1:]
+  
+
+def remove_char_at(str, n):
+    if n < 0:
+        str = str[:]
     else:
-        print("{}".format(chr(i - 32)), end="")
+        str = str[:n] + str[n + 1:]
+    return str
