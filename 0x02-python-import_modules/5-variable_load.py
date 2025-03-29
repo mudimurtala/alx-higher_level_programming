d4ad47166e90f2c5a65197445f749ad285c6dbbd
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-29 15:36:49 +0100

    Review code

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    for i in calculator_1:
+    operations = [add, sub, mul, div]
+    for i in operations:
         print("{} + {} = {}".format(a, b, i(a, b)))
 
 
