76fb21354692f753def85452e7ac3d85ea6bd8c7
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-29 15:47:50 +0100

    Review code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    operations = [add, sub, mul, div]
-    for i in operations:
-        print("{} {} {} = {}".format(a, "?", b, i(a, b)))
+    operations = {add: "+", sub: "-", mul: "*", div: "/"}
+    for i, j in operations.items():
+        print("{} {} {} = {}".format(a, j, b, i(a, b)))
