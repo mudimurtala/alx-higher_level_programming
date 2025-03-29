6a5f55f3c95a57d30ce6317adb1309c96aab9904
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-29 16:35:27 +0100

    Review code

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -6,11 +6,16 @@ if __name__ == "__main__":
 
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    operations = [add, sub, mul, div]
-    for i in operations:
-        print("{} {} {} = {}".format(a, "?", b, i(a, b)))
+    
+    import sys
+
+    argv = sys.argv
+    no_of_argvs = len(argv) - 1
+    if no_of_argvs == 0:
+        print("0 arguments.")
+    elif no_of_argvs == 1:
+        print("1 argument:")
+    else:
+        print("<count> arguments:")
 
 
