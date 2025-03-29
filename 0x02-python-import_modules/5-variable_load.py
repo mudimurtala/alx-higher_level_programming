8b2414bbcde8b32855f4d41d974833d7061ce4aa
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-29 14:17:56 +0100

    Review code

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -4,4 +4,11 @@ if __name__ == "__main__":
     print(a)
 
 
+#!/usr/bin/python3
+from add_0 import add
+a - 1
+b - 2
+add(a, b)
+print("{} + {} = {}".format(a, b, (a + b)))
+if __name__ == "__main__":
 
