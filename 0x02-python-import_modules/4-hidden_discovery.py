137583eac34255009c6ff800908340ae51195aea
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-31 22:22:35 +0100

    Review Solutions

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
 
-import hidden_4
+    items = dir(hidden_4)
 
-items = dir(hidden_4)
-
-items = sorted(items)
-for i in items:
-    if not i.startswith("__"):
-        print(i)
+    items = sorted(items)
+    for i in items:
+        if not i.startswith("__"):
+            print(i)
