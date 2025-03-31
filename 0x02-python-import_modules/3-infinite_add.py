f14c636ca7c70dcdbd319439c29ca2709ce04372
mudimurtala <ibnmuhyideen95@gmail.com>
2025-03-31 22:17:38 +0100

    Review Solutions

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import hidden_4
+    total = 0
 
-items = dir(hidden_4)
-
-items = sorted(items)
-for i in items:
-    if not i.startswith("__"):
-        print(i)
+    for num in sys.argv[1:]:
+        total += int(num)
+    print(total)
