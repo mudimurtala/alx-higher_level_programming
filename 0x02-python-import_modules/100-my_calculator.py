0de616290bad51688be5276e92db24ca7819edf0
mudimurtala <ibnmuhyideen95@gmail.com>
2025-04-01 08:13:55 +0100

    Review Solutions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if (sys.argv[2]) == '+':
+    if sys.argv[2] == '+':
         print("{} {} {} = {}".format(a, '+', b, add(a,b)))
-    elif (sys.argv[2]) == '-':
+    elif sys.argv[2] == '-':
         print("{} {} {} = {}".format(a, '-', b, sub(a,b)))
-    elif (sys.argv[2]) == '*':
+    elif sys.argv[2] == '*':
         print("{} {} {} = {}".format(a, '*', b, mul(a,b)))
     else:
         print("{} {} {} = {}".format(a, '/', b, div(a,b)))
