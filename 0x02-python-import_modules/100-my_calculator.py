db07d97353b68fad0fb97fab5e08ee0e7a4c9a32
mudimurtala <ibnmuhyideen95@gmail.com>
2025-04-01 11:51:55 +0100

    Review Solutions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,23 +3,23 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    operator = ["+", "-", "*", "/"]
+    operators = ["+", "-", "*", "/"]
+    functions = [add, sub, mul, div]
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] not in operator:
+
+    op = sys.argv[2]
+    if sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if sys.argv[2] == '+':
-        print("{} {} {} = {}".format(a, '+', b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print("{} {} {} = {}".format(a, '-', b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print("{} {} {} = {}".format(a, '*', b, mul(a, b)))
-    elif sys.argv[2] == '/':
-        print("{} {} {} = {}".format(a, '/', b, div(a, b)))
+    for i in range(len(operators)):
+        if op == operators[i]:
+            result = functions[i](a, b)
+            print("{} {} {} = {}".format(a, op, b, result))
+            break
