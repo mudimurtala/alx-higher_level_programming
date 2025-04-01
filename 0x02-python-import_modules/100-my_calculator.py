1dc8013cd311276d2e12c225921bdd8dcef270df
mudimurtala <ibnmuhyideen95@gmail.com>
2025-04-01 08:20:03 +0100

    Review Solutions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, '-', b, sub(a,b)))
     elif sys.argv[2] == '*':
         print("{} {} {} = {}".format(a, '*', b, mul(a,b)))
-    else:
+    elif sys.argv[2] == '/':
         print("{} {} {} = {}".format(a, '/', b, div(a,b)))
