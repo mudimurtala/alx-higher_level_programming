f0fb872f1e267692be674b4c8fadca88b264acc7
mudimurtala <ibnmuhyideen95@gmail.com>
2025-04-01 08:12:01 +0100

    Review Solutions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,4 +2,23 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    print(sys.argv)
+    operator = ["+", "-", "*", "/"]
+
+    if len(sys.argv) != 4:
+        print('More than three arguments')
+        sys.exit(1)
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if (sys.argv[2]) == '+':
+        print("{} {} {} = {}".format(a, '+', b, add(a,b)))
+    elif (sys.argv[2]) == '-':
+        print("{} {} {} = {}".format(a, '-', b, sub(a,b)))
+    elif (sys.argv[2]) == '*':
+        print("{} {} {} = {}".format(a, '*', b, mul(a,b)))
+    else:
+        print("{} {} {} = {}".format(a, '/', b, div(a,b)))
