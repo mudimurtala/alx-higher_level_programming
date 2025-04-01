30a8e7c967d582878c1e39758969d1511d229783
mudimurtala <ibnmuhyideen95@gmail.com>
2025-04-01 04:39:31 +0100

    Review Solutions

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,39 +1,4 @@
 #!/usr/bin/python3
-
-
-# if __name__ == "__main__":
-#     from variable_load_5 import a
-#     print(a)
-
-
-# #!/usr/bin/python3
-# if __name__ == "__main__":
-#     import sys
-
-#     total = 0
-
-#     for num in sys.argv[1:]:
-#         total += int(num)
-#     print(total)
-
-
-# open hidden_4.pyc
-# loop for all the elements in the file
-# if name starts with'_':
-# ignore it
-# else:
-# arrange them in alphabetical order
-# print them one name per line
-
-
-
-
-#!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    total = 0
-
-    for num in sys.argv[1:]:
-        total += int(num)
-    print(total)
+    from variable_load_5 import a
+    print(a)
