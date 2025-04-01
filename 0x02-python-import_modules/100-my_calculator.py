5367a9a1545cc514608d47cce9bf00969856089f
mudimurtala <ibnmuhyideen95@gmail.com>
2025-04-01 08:35:42 +0100

    Review Solutions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,15 +11,15 @@ if __name__ == "__main__":
     if sys.argv[2] not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
     if sys.argv[2] == '+':
-        print("{} {} {} = {}".format(a, '+', b, add(a,b)))
+        print("{} {} {} = {}".format(a, '+', b, add(a, b)))
     elif sys.argv[2] == '-':
-        print("{} {} {} = {}".format(a, '-', b, sub(a,b)))
+        print("{} {} {} = {}".format(a, '-', b, sub(a, b)))
     elif sys.argv[2] == '*':
-        print("{} {} {} = {}".format(a, '*', b, mul(a,b)))
+        print("{} {} {} = {}".format(a, '*', b, mul(a, b)))
     elif sys.argv[2] == '/':
-        print("{} {} {} = {}".format(a, '/', b, div(a,b)))
+        print("{} {} {} = {}".format(a, '/', b, div(a, b)))
